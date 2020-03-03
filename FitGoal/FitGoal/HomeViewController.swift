86b9a9e46e3ae12c89c978213c1a1ca5c74b90dd
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-03-04 10:22:41 +1100

    Suggestions added

--- FitGoal/FitGoal/HomeViewController.swift
+++ FitGoal/FitGoal/HomeViewController.swift
@@ -10,34 +10,44 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        configureBackgroundView()
+    var topView: UIView!
+    var gradient: CAGradientLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackground()
+        gradient = createGradient()
+        topView.layer.addSublayer(gradient)
+        
+        self.view.addSubview(topView)
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradient.frame = topView.bounds
     }
 }
 
 extension HomeViewController {
-    private func configureBackgroundView() {
-         view.backgroundColor = #colorLiteral(red: 0.9647058824, green: 0.9647058824, blue: 0.9647058824, alpha: 1)
-         let backgroundView = UIView(frame: CGRect(x: 0, y: 0, width: 550, height: 812))
-         backgroundView.layer.masksToBounds = true
-         backgroundView.layer.cornerRadius = 150
-         backgroundView.layer.maskedCorners = [.layerMinXMaxYCorner]
-         backgroundView.transform = backgroundView.transform.rotated(by: -0.4537856055185257)
-         backgroundView.transform = backgroundView.transform.translatedBy(x: 10, y: -600)
-
-         view.addSubview(backgroundView)
-         createGradient(in: backgroundView)
-     }
-
-     private func createGradient(in view: UIView) {
-         let gradient = CAGradientLayer()
-         gradient.frame = view.bounds
-         let fistColour = #colorLiteral(red: 0.2816967666, green: 0.8183022738, blue: 0.9222241044, alpha: 1)
-         let secondColour = #colorLiteral(red: 0.5647058824, green: 0.07450980392, blue: 0.9568627451, alpha: 1)
-         gradient.colors = [fistColour.cgColor, secondColour.cgColor]
-         gradient.locations = [0, 1]
-         view.layer.insertSublayer(gradient, at: 0)
-     }
+    private func configureBackground() {
+        self.view.backgroundColor = #colorLiteral(red: 0.9647058824, green: 0.9647058824, blue: 0.9647058824, alpha: 1)
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 600, height: 812))
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 150
+        view.layer.maskedCorners = [.layerMinXMaxYCorner]
+        let rotation = CGAffineTransform(rotationAngle: -26 / 180 * CGFloat.pi)
+        let transform = rotation.translatedBy(x: 10, y: -600)
+        view.transform = transform
+        self.topView = view
+    }
+    
+    private func createGradient() -> CAGradientLayer {
+        let gradient = CAGradientLayer()
+        let fistColour = #colorLiteral(red: 0.2816967666, green: 0.8183022738, blue: 0.9222241044, alpha: 1)
+        let secondColour = #colorLiteral(red: 0.5647058824, green: 0.07450980392, blue: 0.9568627451, alpha: 1)
+        gradient.colors = [fistColour.cgColor, secondColour.cgColor]
+        gradient.locations = [0, 1]
+        return gradient
+    }
 }
 
