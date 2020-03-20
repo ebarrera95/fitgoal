183a9ced49d38a54e6298dd0a1cea0e9dbaff900
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-03-20 14:41:49 +1100

    Create SingUpVC: This VC will be in charge of the singUp process

---
+++ FitGoal/FitGoal/Singup&Singin/SignUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewController.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 20/3/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+  private var backgroundView = BackgroundView()
+}
