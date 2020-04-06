8524a6e21be7c58a82a2dc3a5d2c57616f50fab7
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-04-06 16:52:58 +1000

    Refactor de func to remove repeated logic and manage errors

--- FitGoal/FitGoal/Singup&Singin/SocialMediaAuthenticator.swift
+++ FitGoal/FitGoal/Singup&Singin/SocialMediaAuthenticator.swift
@@ -72,23 +72,41 @@ class SocialMediaAuthenticator: NSObject, GIDSignInDelegate {
     
     //MARK: - Facebook
     
-    
     private func facebookSignIn(sender: UIViewController, completion: @escaping SignInCallback) {
         let permissions = ["public_profile", "email"]
         LoginManager().logIn(permissions: permissions, from: sender) { (logInResuts, error) in
-            if let error = error { completion(.failure(error)) }
+            if let error = error {
+                completion(.failure(error))
+            }
             else {
-                guard let logInResuts = logInResuts else { return }
+                guard let logInResuts = logInResuts else {
+                    let error = LoginError.noLogInResultsFound
+                    completion(.failure(error))
+                    return
+                }
+                
                 if logInResuts.isCancelled {
                     let error = LoginError.userCanceledLogIn
                     completion(.failure(error))
                 }
                 else {
-                    guard let user = Auth.auth().currentUser else { return }
-                    guard let email = user.email else { return }
-                    Auth.auth().fetchSignInMethods(forEmail: email) { (signInmethods, error) in
-                        if let error = error { completion(.failure(error)) }
-                        else if signInmethods != nil {
+                    guard let user = Auth.auth().currentUser else {
+                        let error = MissingUserInfoError.noUserFound
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    guard let email = user.email else {
+                        let error = MissingUserInfoError.noEmailFound
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    Auth.auth().fetchSignInMethods(forEmail: email) { (signInMethods, error) in
+                        if let error = error {
+                            completion(.failure(error))
+                        }
+                        else if signInMethods != nil && !signInMethods!.contains("facebook.com") {
                             self.mergeNewUserWith(currentUser: user, completion: completion)
                         } else {
                             self.createNewUser(completion: completion)
@@ -98,44 +116,73 @@ class SocialMediaAuthenticator: NSObject, GIDSignInDelegate {
             }
         }
     }
+    
     private func mergeNewUserWith(currentUser: User, completion: @escaping SignInCallback) {
-        guard let credentials = getFacebookCredentials() else { return }
-        currentUser.link(with: credentials) { (dataResults, error) in
-            if let error = error {
-                completion(.failure(error))
+        do {
+            let credentials = try getFacebookCredentials()
+            currentUser.link(with: credentials) { (dataResults, error) in
+                self.manageResutls(dataResults: dataResults, error: error, completion: completion)
             }
-            self.createUser(with: dataResults, completion: completion)
+        }
+        catch {
+            completion(.failure(error))
         }
     }
     
     private func createNewUser(completion: @escaping SignInCallback) {
-        guard let credentials = getFacebookCredentials() else { return }
-        Auth.auth().signIn(with: credentials) { ( dataResults, error) in
-            if let error = error {
+        do {
+            let credentials = try getFacebookCredentials()
+            Auth.auth().signIn(with: credentials) { ( dataResults, error) in
+                self.manageResutls(dataResults: dataResults, error: error, completion: completion)
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    /*
+     I've moved this part of the logic to an independent func to avoid repeating code. Not sure if it's the best way
+     to manage this because when you read the funcs `createNewUser` and `mergeNewUserWith` it's not clear when the
+     `completion(.success)` casewill happen
+     */
+    private func manageResutls(dataResults: AuthDataResult?, error: Error?, completion: @escaping SignInCallback) {
+        if let error = error {
+            completion(.failure(error))
+        } else {
+            do {
+                guard let user = dataResults?.user else {
+                    let error = MissingUserInfoError.noUserFound
+                    completion(.failure(error))
+                    return
+                }
+                let userInfo = try self.create(user: user)
+                self.appPreferences.loggedInUser = userInfo
+                completion(.success(()))
+            } catch {
                 completion(.failure(error))
             }
-            self.createUser(with: dataResults, completion: completion)
         }
     }
     
-    private func getFacebookCredentials() -> AuthCredential? {
-        guard let accessToken = AccessToken.current else { return nil }
+    private func getFacebookCredentials() throws -> AuthCredential {
+        guard let accessToken = AccessToken.current else { throw LoginError.noAuthCredentialsFound }
         return FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
     }
     
-    private func createUser(with dataResults: AuthDataResult?, completion: @escaping SignInCallback) {
-        guard let user = dataResults?.user else { return }
-        guard let name = user.displayName else { return }
-        guard let email = user.email else { return }
-        let userInfo = UserInformation(name: name, email: email)
-        self.appPreferences.loggedInUser = userInfo
-        completion(.success(()))
+    private func create(user: User) throws  -> UserInformation {
+        guard let name = user.displayName else { throw MissingUserInfoError.noNameFound }
+        guard let email = user.email else { throw MissingUserInfoError.noEmailFound }
+        return UserInformation(name: name, email: email)
     }
 }
 
 
 private enum LoginError: Error {
     case userCanceledLogIn
+    case noAuthCredentialsFound
+    case noLogInResultsFound
+}
+
+private enum MissingUserInfoError: Error {
     case noUserFound
     case noNameFound
     case noEmailFound
