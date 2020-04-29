8cf13ab675872579e3f8b517aa000ed28d7c2a97
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-04-29 10:29:30 +1000

    fix init

--- FitGoal/FitGoal/UserProfileConfiguration/GenderView.swift
+++ FitGoal/FitGoal/UserProfileConfiguration/GenderView.swift
@@ -10,8 +10,8 @@ import Foundation
 import UIKit
 
 class GenderView: UIView {
-    let female = IconBuilderView(icon: .init(icon: .female))
-    let male = IconBuilderView(icon: .init(icon: .male))
+    let female = WalkthroughIconView(icon: WalkthroughIcon(iconType: .female))
+    let male = WalkthroughIconView(icon: WalkthroughIcon(iconType: .male)) 
     
     override init(frame: CGRect) {
         super.init(frame: frame)
