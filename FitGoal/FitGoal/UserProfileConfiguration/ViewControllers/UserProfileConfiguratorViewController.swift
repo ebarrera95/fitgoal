3504c2fd0b234b000408028f73196b7a08481a56
ebarrera95 <50035462+ebarrera95@users.noreply.github.com>
2020-05-04 19:06:37 +1000

    User profile page controller (#22)
    * Images
    
    * Modify this temporarily to test code
    
    * These are the old files I was working on. These files will suffer modifications in the future as I introduce the things I've learnt from Rey
    
    * Same as last comment
    
    * BodyShapeSelectionView
    
    * Rename icons
    
    * Refactor Body shape to match a struct instead of a enum
    
    * Rename constraint functions. This case the AxisConstraints are set apart from the DimensionConstraints to not repeat code
    
    * Move BodyLevelView to new file
    
    * introduce IconBuilderViewDelegate
    
    * Change the state of the icon and selection
    
    * Getting ready for PR
    
    * Create UIPageViewController with two VC, one to select BodyShape and the other to select gender. VCs are not complete yet, only the main image is on display
    
    * User gender and fitness level ready
    
    * Move labels to child VC
    
    * fitness level, goal and gender
    
    * UIPageViewControllerWorking well
    
    * Create new view so manage when the age weight and height user info
    
    * Initial configuration
    
    * Change textField contraints
    
    * fix init
    
    * remove .uppercased() o suffix
    
    * Delete quantitativeView so PR is shorter
    
    * Getting ready for PR, renaming objects
    
    * More on renaming and moving code around so is easier to read
    
    * Delete files
    
    * Delete file
    
    * Renaming
    
    * Space
    
    * PR Changes: Modify VC so it doesn't use an enum case in its init. VC will receive a view a two strings.
    
    * Space
    
    * replace var with let
    
    * delete logic to calculate view height

---
+++ FitGoal/FitGoal/UserProfileConfiguration/ViewControllers/UserProfileConfiguratorViewController.swift
@@ -0,0 +1,75 @@
+//
+//  UserGenderViewController.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 26/4/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileConfiguratorViewController: UIViewController {
+    
+    private let selectorView: UIView
+    private let questionPrefix = UILabel()
+    private let questionSuffix = UILabel()
+
+    init(selectorView: UIView, questionPrefix: String, questionSuffix: String) {
+        self.selectorView = selectorView
+        super.init(nibName: nil, bundle: nil)
+        configureQuestions(prefix: questionPrefix, suffix: questionSuffix)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureQuestions(prefix: String, suffix: String) {
+        questionPrefix.attributedText = prefix.formattedText(font: "Roboto-Light", size: 19, color: .white, kern: 0.23)
+        questionSuffix.attributedText = suffix.formattedText(font: "Oswald-Medium", size: 50, color: .white, kern: 0.59)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let views = [
+            selectorView,
+            questionPrefix,
+            questionSuffix
+        ]
+        
+        view.addMultipleSubviews(views)
+        setConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        selectorView.frame = CGRect(x: 0, y: 0, width: 320, height: 320)
+        selectorView.center = CGPoint(x: view.center.x, y: view.center.y + 30)
+    }
+    
+    //MARK: -Constraints
+    private func setConstraints() {
+        setQuestionPrefixLabelConstraints()
+        setQuestionSuffixLabelConstraints()
+    }
+    
+    private func setQuestionPrefixLabelConstraints() {
+        questionPrefix.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            questionPrefix.bottomAnchor.constraint(equalTo: self.questionSuffix.topAnchor),
+            questionPrefix.leadingAnchor.constraint(equalTo: self.selectorView.leadingAnchor)
+        ])
+    }
+    
+    private func setQuestionSuffixLabelConstraints() {
+        questionSuffix.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            questionSuffix.bottomAnchor.constraint(equalTo: selectorView.topAnchor, constant: -16),
+            questionSuffix.leadingAnchor.constraint(equalTo: selectorView.leadingAnchor)
+        ])
+    }
+}
---
+++ FitGoal/FitGoal/UserProfileConfiguration/ViewControllers/WalkthroughPageViewController.swift
@@ -0,0 +1,169 @@
+//
+//  UserProfilePageViewController.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 26/4/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    private var pendingIndex = Int()
+    private var currentIndex = Int()
+    
+    private lazy var gradientBackgroundView: UIView = {
+        let gradientView = GradientView(frame: CGRect(x: 0, y: 0, width: 800, height: 812))
+        gradientView.layer.cornerRadius = 150
+        gradientView.layer.maskedCorners = [.layerMinXMaxYCorner]
+        gradientView.colors = [#colorLiteral(red: 0.2816967666, green: 0.8183022738, blue: 0.9222241044, alpha: 1), #colorLiteral(red: 0.5647058824, green: 0.07450980392, blue: 0.9568627451, alpha: 1)]
+        let rotation = CGAffineTransform(rotationAngle: -26 / 180 * CGFloat.pi)
+        gradientView.transform = rotation.translatedBy(x: -130, y: -250)
+        return gradientView
+    }()
+    
+    private let createYourProfileLabel: UILabel = {
+        let label = UILabel()
+        let title = "Create your Profile".uppercased()
+        label.attributedText = title.formattedText(font: "Oswald-Medium", size: 18, color: .white, kern: 0.50)
+        return label
+    }()
+    
+    private let nextViewControllerButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(imageLiteralResourceName: "next"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2884573063)
+        button.layer.shadowOffset = CGSize(width: 0, height: 1)
+        button.layer.shadowRadius = 2
+        button.layer.shadowOpacity = 1
+        return button
+    }()
+    
+    private let walkthroughViewControllers: [UIViewController] = {
+        let gender = UserProfileConfiguratorViewController(
+            selectorView: GenderView(),
+            questionPrefix: "What is",
+            questionSuffix: "your gender".uppercased()
+        )
+        
+        let fitnessLevel = UserProfileConfiguratorViewController(
+            selectorView: FitnessLevelChooserView(),
+            questionPrefix: "What is your current fitness",
+            questionSuffix: "level".uppercased()
+        )
+        
+        let fitnessGoal = UserProfileConfiguratorViewController(
+            selectorView: FitnessLevelChooserView(),
+            questionPrefix: "What is",
+            questionSuffix: "your goal".uppercased()
+        )
+        
+        return [gender, fitnessLevel, fitnessGoal]
+    }()
+    
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = walkthroughViewControllers.count
+        pageControl.currentPage = currentIndex
+        pageControl.pageIndicatorTintColor = .white
+        pageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0.2431372549, green: 0.7803921569, blue: 0.9019607843, alpha: 1)
+        return pageControl
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+        
+        view.backgroundColor = #colorLiteral(red: 0.9647058824, green: 0.9647058824, blue: 0.9647058824, alpha: 1)
+        view.insertSubview(gradientBackgroundView, at: 0)
+        
+        let views = [
+            createYourProfileLabel,
+            nextViewControllerButton,
+            pageControl
+        ]
+        
+        self.view.addMultipleSubviews(views)
+        
+        self.setViewControllers([walkthroughViewControllers[currentIndex]], direction: .forward, animated: true, completion: nil)
+        setConstraints()
+        
+        nextViewControllerButton.addTarget(self, action: #selector(nextViewController), for: .touchUpInside)
+    }
+    
+    @objc private func nextViewController() {
+        if (currentIndex + 1) < walkthroughViewControllers.count {
+            currentIndex += 1
+            pageControl.currentPage = currentIndex
+            self.setViewControllers([walkthroughViewControllers[currentIndex]], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    private func setConstraints() {
+        setLabelConstraints()
+        setButtonConstraints()
+        setPageControlConstraints()
+    }
+    
+    private func setLabelConstraints() {
+        createYourProfileLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            createYourProfileLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            createYourProfileLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50)
+        ])
+    }
+    
+    private func setButtonConstraints() {
+        nextViewControllerButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextViewControllerButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -30),
+            nextViewControllerButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
+            nextViewControllerButton.heightAnchor.constraint(equalToConstant: 72),
+            nextViewControllerButton.widthAnchor.constraint(equalToConstant: 72)
+        ])
+    }
+    
+    private func setPageControlConstraints() {
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pageControl.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+            pageControl.topAnchor.constraint(equalTo: createYourProfileLabel.topAnchor)
+        ])
+    }
+}
+
+extension WalkthroughPageViewController {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = walkthroughViewControllers.firstIndex(of: viewController) else { return nil }
+        guard !(currentIndex == 0) else { return nil }
+        let previousIndex = currentIndex - 1
+        return walkthroughViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = walkthroughViewControllers.firstIndex(of: viewController) else { return nil }
+        guard !(currentIndex == walkthroughViewControllers.count - 1) else { return nil }
+        let nextIndex = currentIndex + 1
+        return walkthroughViewControllers[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        guard let vc = pendingViewControllers.first else { return }
+        guard let index = walkthroughViewControllers.firstIndex(of: vc) else { return }
+        pendingIndex = index
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            currentIndex = pendingIndex
+            pageControl.currentPage = currentIndex
+        }
+    }
+}
