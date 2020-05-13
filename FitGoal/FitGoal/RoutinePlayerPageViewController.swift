033608ebbd467df4b0ccc7a4050cc4ffc98df2d3
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-05-13 13:54:34 +1000

    Create Page viewController

---
+++ FitGoal/FitGoal/RoutinePlayerPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RoutinePlayerPageViewController.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 13/5/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import UIKit
+
+class RoutinePlayerPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
