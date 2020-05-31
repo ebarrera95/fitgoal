f00726738ddee49288a82b1928c89c9ffdbb056d
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-05-31 19:06:00 +1000

    UITextFieldDelegateImplementation

--- FitGoal/FitGoal/UserProfileConfiguration/IconListViewDelegate.swift
+++ FitGoal/FitGoal/UserProfileConfiguration/IconListViewDelegate.swift
@@ -6,4 +6,58 @@
 //  Copyright © 2020 Eliany Barrera. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class IconListDelegate: IconListViewDelegate {
+    
+    private let appPreferences = AppPreferences()
+    private let userInfo: QualitativeUserInfo
+    
+    init(listView: IconListView, qualitativeUserInfo: QualitativeUserInfo) {
+        self.userInfo = qualitativeUserInfo
+        listView.delegate = self
+    }
+    
+    func iconListView(_ listView: IconListView, didSelectIcon iconView: WalkthroughIconView) {
+        switch userInfo {
+        case .fitnessGoal:
+            switch iconView.icon.iconType {
+            case .fitnessLevel(let fitnessLevel):
+                appPreferences.fitnessGoal = fitnessLevel
+            default:
+                return
+            }
+        case .currentFitnessLevel:
+            switch iconView.icon.iconType {
+            case .fitnessLevel(let fitnessLevel):
+                appPreferences.fitnessLevel = fitnessLevel
+            default:
+                return
+            }
+        case .gender:
+            switch iconView.icon.iconType {
+            case .gender(let gender):
+                appPreferences.userGender = gender
+            default:
+                return
+            }
+        }
+    }
+    
+    func iconListView(_ listView: IconListView, didDeselectIcon iconView: WalkthroughIconView) {
+        switch userInfo {
+        case .fitnessGoal:
+            appPreferences.fitnessGoal = nil
+        case .currentFitnessLevel:
+            appPreferences.fitnessLevel = nil
+        case .gender:
+            appPreferences.userGender = nil
+        }
+    }
+}
+
+enum QualitativeUserInfo {
+    case gender
+    case currentFitnessLevel
+    case fitnessGoal
+}
---
+++ FitGoal/FitGoal/UserProfileConfiguration/UserInfoTextFieldDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  UserInfoTextFieldDelegate.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 31/5/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    private let appPreferences = AppPreferences()
+    private let userInfo: QuantitativeUserInfo
+    
+    init(textField: UITextField, quantitativeUserInfo: QuantitativeUserInfo) {
+        self.userInfo = quantitativeUserInfo
+        super.init()
+        textField.delegate = self
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch userInfo {
+        case .age:
+            if let text = textField.text, let age = Int(text) {
+                appPreferences.age = age
+            }
+        case .height:
+            if let text = textField.text, let height = Int(text) {
+                appPreferences.height = height
+            }
+        case .weight:
+            if let text = textField.text, let weight = Int(text) {
+                appPreferences.weight = weight
+            }
+        }
+    }
+}
+
+enum QuantitativeUserInfo {
+    case age
+    case height
+    case weight
+}
