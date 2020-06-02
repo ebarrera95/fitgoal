33f3f87c93ddde6c6b97951e260daae1f7ae90e7
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-06-02 11:17:06 +1000

    Conform types to CaseIterable Protocol

---
+++ FitGoal/FitGoal/Setting/GenderPickerViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsUserInfoEditor.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 2/6/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import Foundation
+
+class SettingsUserInfoEditor {
+    
+}
