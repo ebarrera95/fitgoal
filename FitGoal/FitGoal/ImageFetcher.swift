149fed385186d4df39aa3ce204026988dad1d86a
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-06-04 08:40:24 +1000

    Move main func and properties for imageFetcher and create delegate

--- FitGoal/FitGoal/ImageFetcher.swift
+++ FitGoal/FitGoal/ImageFetcher.swift
@@ -9,3 +9,43 @@
 import UIKit
 
 var imageCache: [URL: UIImage] = [:]
+
+protocol ImageFetcherDelegate: AnyObject {
+    func imageFetcherIsInProgress()
+    
+    func imageFetcher(_ imageFetcher: ImageFetcher, didFinishFetchingWith image: UIImage)
+    
+    func imageFetcher(_ imageFetcher: ImageFetcher, failedWith error: Error)
+}
+
+class ImageFetcher {
+    
+    weak var delegate: ImageFetcherDelegate?
+    
+    private var imageLoadingState: ImageLoadingState = .inProgress {
+        didSet {
+            switch imageLoadingState {
+            case .inProgress:
+                delegate?.imageFetcherIsInProgress()
+            case .finished(let image):
+                delegate?.imageFetcher(self, didFinishFetchingWith: image)
+            case .failed(let error):
+                delegate?.imageFetcher(self, failedWith: error)
+            }
+        }
+    }
+    
+    private func fetchImage(with imageURL: URL) {
+        imageURL.fetchImage { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    self.imageLoadingState = .failed(error)
+                case .success(let image):
+                    imageCache[imageURL] = image
+                    self.imageLoadingState = .finished(image)
+                }
+            }
+        }
+    }
+}
