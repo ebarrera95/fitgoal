1213f70dc7bf815f7716f3e9b10a09ec01dcee5f
Eliany Barrera <eliany.barrera.alvarez@gmail.com>
2020-06-06 08:15:57 +1000

    Delete protocol (It wasn't adding any good)

---
+++ FitGoal/FitGoal/ExercisePlayerBackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  ExercisePlayerBackgroundView.swift
+//  FitGoal
+//
+//  Created by Eliany Barrera on 6/6/20.
+//  Copyright © 2020 Eliany Barrera. All rights reserved.
+//
+
+import UIKit
+
+class ExercisePlayerBackgroundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
